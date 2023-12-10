73d0062591acdc14e5f654fe1c1e13ced309b906
Bongani Nyoni <b.nyoni1101@gmail.com>
2023-12-10 09:59:48 +0200

    first commit

---
+++ scripts/after_install.sh
@@ -0,0 +1,3 @@
+#!bin/bash
+cd /home/ubuntu/my-app/react-cicd-app
+sudo cp _R build/ /var/www/html/react-cicd-app/
---
+++ scripts/before_install.sh
@@ -0,0 +1,4 @@
+#!bin/bash
+cd /home/ubuntu/my-app/react-cicd-app
+curl -fsSL https://deb.nodesource.com/setup_20.x | sudo -E bash -
+sudo apt install -y nodejs
